37d55fb3afce21996aea9c760203b2658ec1a152
VictorMeyer77 <victormeyerc2i@gmail.com>
2024-09-23 16:43:59 +0200

    network: implement try_from for TlsContentType & TlsVersion

--- network/src/capture/application/tls.rs
+++ network/src/capture/application/tls.rs
@@ -2,7 +2,9 @@ use crate::error::Error;
 use nom::bytes::complete::take;
 use nom::number::complete::be_u16;
 use nom::IResult;
+use std::convert::TryFrom;
 
+#[repr(u8)]
 #[derive(Debug, Clone, PartialEq)]
 pub enum TlsContentType {
     ChangeCipherSpec = 20,
@@ -12,6 +14,7 @@ pub enum TlsContentType {
     Heartbeat = 24,
 }
 
+#[repr(u16)]
 #[derive(Debug, Clone, PartialEq)]
 pub enum TlsVersion {
     SSL20 = 2,
@@ -30,30 +33,46 @@ pub struct Tls {
     pub payload: Vec<u8>,
 }
 
-impl TlsContentType {
-    fn from_u8(byte: u8) -> Option<Self> {
+impl TryFrom<u8> for TlsContentType {
+    type Error = ();
+
+    fn try_from(byte: u8) -> Result<Self, Self::Error> {
         match byte {
-            0x14 => Some(TlsContentType::ChangeCipherSpec),
-            0x15 => Some(TlsContentType::Alert),
-            0x16 => Some(TlsContentType::Handshake),
-            0x17 => Some(TlsContentType::ApplicationData),
-            0x18 => Some(TlsContentType::Heartbeat),
-            _ => None,
+            0x14 => Ok(TlsContentType::ChangeCipherSpec),
+            0x15 => Ok(TlsContentType::Alert),
+            0x16 => Ok(TlsContentType::Handshake),
+            0x17 => Ok(TlsContentType::ApplicationData),
+            0x18 => Ok(TlsContentType::Heartbeat),
+            _ => Err(()),
         }
     }
 }
 
-impl TlsVersion {
-    fn from_u16(version: u16) -> Option<Self> {
+impl From<TlsContentType> for u8 {
+    fn from(content_type: TlsContentType) -> Self {
+        content_type as u8
+    }
+}
+
+impl TryFrom<u16> for TlsVersion {
+    type Error = ();
+
+    fn try_from(version: u16) -> Result<Self, Self::Error> {
         match version {
-            2 => Some(TlsVersion::SSL20),
-            768 => Some(TlsVersion::SSL30),
-            769 => Some(TlsVersion::TLS10),
-            770 => Some(TlsVersion::TLS11),
-            771 => Some(TlsVersion::TLS12),
-            772 => Some(TlsVersion::TLS13),
-            _ => None,
+            2 => Ok(TlsVersion::SSL20),
+            768 => Ok(TlsVersion::SSL30),
+            769 => Ok(TlsVersion::TLS10),
+            770 => Ok(TlsVersion::TLS11),
+            771 => Ok(TlsVersion::TLS12),
+            772 => Ok(TlsVersion::TLS13),
+            _ => Err(()),
         }
+    }
+}
+
+impl From<TlsVersion> for u16 {
+    fn from(version: TlsVersion) -> Self {
+        version as u16
     }
 }
 
@@ -67,17 +86,16 @@ impl Tls {
     }
 
     fn new(content_type: u8, version: u16, length: u16, payload: &[u8]) -> Result<Self, Error> {
-        if let Some(content_type) = TlsContentType::from_u8(content_type) {
-            if let Some(version) = TlsVersion::from_u16(version) {
-                return Ok(Tls {
-                    content_type,
-                    version,
-                    length,
-                    payload: payload.to_vec(),
-                });
-            }
-        }
-        Err(Error::PacketParsing)
+        let content_type: TlsContentType =
+            TlsContentType::try_from(content_type).map_err(|_| Error::PacketParsing)?;
+        let version: TlsVersion =
+            TlsVersion::try_from(version).map_err(|_| Error::PacketParsing)?;
+        Ok(Tls {
+            content_type,
+            version,
+            length,
+            payload: payload.to_vec(),
+        })
     }
 
     fn parse_header_bytes(bytes: &[u8]) -> IResult<&[u8], (u8, u16, u16)> {
@@ -93,36 +111,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_tls_content_type_from_u8() {
+    fn test_tls_content_type_try_from() {
         assert_eq!(
-            TlsContentType::from_u8(0x14),
-            Some(TlsContentType::ChangeCipherSpec)
-        );
-        assert_eq!(TlsContentType::from_u8(0x15), Some(TlsContentType::Alert));
-        assert_eq!(
-            TlsContentType::from_u8(0x16),
-            Some(TlsContentType::Handshake)
+            TlsContentType::try_from(0x14).unwrap(),
+            TlsContentType::ChangeCipherSpec
         );
         assert_eq!(
-            TlsContentType::from_u8(0x17),
-            Some(TlsContentType::ApplicationData)
+            TlsContentType::try_from(0x15).unwrap(),
+            TlsContentType::Alert
         );
         assert_eq!(
-            TlsContentType::from_u8(0x18),
-            Some(TlsContentType::Heartbeat)
+            TlsContentType::try_from(0x16).unwrap(),
+            TlsContentType::Handshake
         );
-        assert_eq!(TlsContentType::from_u8(0x19), None);
+        assert_eq!(
+            TlsContentType::try_from(0x17).unwrap(),
+            TlsContentType::ApplicationData
+        );
+        assert_eq!(
+            TlsContentType::try_from(0x18).unwrap(),
+            TlsContentType::Heartbeat
+        );
+        assert!(TlsContentType::try_from(0x19).is_err());
     }
 
     #[test]
-    fn test_tls_version_from_u16() {
-        assert_eq!(TlsVersion::from_u16(2), Some(TlsVersion::SSL20));
-        assert_eq!(TlsVersion::from_u16(768), Some(TlsVersion::SSL30));
-        assert_eq!(TlsVersion::from_u16(769), Some(TlsVersion::TLS10));
-        assert_eq!(TlsVersion::from_u16(770), Some(TlsVersion::TLS11));
-        assert_eq!(TlsVersion::from_u16(771), Some(TlsVersion::TLS12));
-        assert_eq!(TlsVersion::from_u16(772), Some(TlsVersion::TLS13));
-        assert_eq!(TlsVersion::from_u16(773), None);
+    fn test_tls_version_try_from() {
+        assert_eq!(TlsVersion::try_from(2).unwrap(), TlsVersion::SSL20);
+        assert_eq!(TlsVersion::try_from(768).unwrap(), TlsVersion::SSL30);
+        assert_eq!(TlsVersion::try_from(769).unwrap(), TlsVersion::TLS10);
+        assert_eq!(TlsVersion::try_from(770).unwrap(), TlsVersion::TLS11);
+        assert_eq!(TlsVersion::try_from(771).unwrap(), TlsVersion::TLS12);
+        assert_eq!(TlsVersion::try_from(772).unwrap(), TlsVersion::TLS13);
+        assert!(TlsVersion::try_from(773).is_err());
     }
 
     #[test]
