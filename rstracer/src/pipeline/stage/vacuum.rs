bf1be329fd16837d5c2ae50120df43c1ca97c10f
VictorMeyer77 <victormeyerc2i@gmail.com>
2024-09-25 14:44:03 +0200

    rstracer: remove transaction for vacuum

--- rstracer/src/pipeline/stage/vacuum.rs
+++ rstracer/src/pipeline/stage/vacuum.rs
@@ -2,20 +2,18 @@ use crate::config::VacuumConfig;
 use crate::pipeline::stage::schema::Schema;
 
 pub fn vacuum_request(config: VacuumConfig, schema: Schema) -> String {
-    let mut query: String = "BEGIN;".to_string();
+    let mut query: String = "".to_string();
 
     for table in schema.tables {
         for layer in config.to_list() {
             if table.name.starts_with(&layer.0) && layer.1 > 0 {
                 query.push_str(&format!(
-                    "DELETE FROM memory.{} WHERE inserted_at + '{} seconds' < current_timestamp;",
+                    "DELETE FROM memory.{} WHERE inserted_at + '{} seconds' < CURRENT_TIMESTAMP;",
                     table.name, layer.1
                 ));
             }
         }
     }
-
-    query.push_str("COMMIT;");
 
     query
 }
