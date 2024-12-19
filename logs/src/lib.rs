1f283d727fc31d32ff9421e864f38c30ab828837
victor <victormeyerc2i@gmail.com>
2024-12-19 17:57:19 +0100

    logs: set producer async & add test

--- logs/src/lib.rs
+++ logs/src/lib.rs
@@ -3,11 +3,15 @@ pub mod error;
 use crate::error::Error;
 use regex::Captures;
 use regex::Regex;
-use std::io::{BufRead, BufReader};
-use std::process::{ChildStdout, Command, Stdio};
+use std::process::Stdio;
+
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::time::Duration;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::{ChildStdout, Command};
 use tokio::sync::mpsc::Sender;
+use tokio::time::timeout;
 
 const ROW_REGEX: &str = r"(\d+\.\d+)\s+(\S+)\s+(\S+)\[(\d+)\]:\s+(.+)";
 
@@ -50,14 +54,16 @@ pub async fn producer(
 ) -> Result<(), Error> {
     let reader = read_journal_cmd()?;
     let regex = Regex::new(ROW_REGEX)?;
-    let mut raw_logs = reader.lines().map_while(Result::ok);
+    let mut raw_logs = reader.lines();
 
     while !stop_flag.load(Ordering::Relaxed) {
-        if let Some(raw_log) = raw_logs.next() {
-            if let Some(captures) = regex.captures(&raw_log) {
-                let log = JournalLog::from(captures)?;
-                if let Err(e) = sender.send(log).await {
-                    return Err(Error::Channel(Box::new(e)));
+        if let Ok(raw_log) = timeout(Duration::from_millis(1000), raw_logs.next_line()).await {
+            if let Some(raw_log) = raw_log? {
+                if let Some(captures) = regex.captures(&raw_log) {
+                    let log = JournalLog::from(captures)?;
+                    if let Err(e) = sender.send(log).await {
+                        return Err(Error::Channel(Box::new(e)));
+                    }
                 }
             }
         }
@@ -69,6 +75,10 @@ pub async fn producer(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Duration;
+    use tokio::join;
+    use tokio::sync::mpsc::{channel, Receiver};
+    use tokio::time::sleep;
 
     #[test]
     fn test_journal_log_from_captures() {
@@ -91,6 +101,41 @@ mod tests {
                     "pam_unix(sudo:session): session opened for user root(uid=0) by (uid=1000)"
                         .to_string(),
             }
+        );
+    }
+
+    #[tokio::test]
+    async fn test_producer() {
+        let (sender, mut receiver): (Sender<JournalLog>, Receiver<JournalLog>) = channel(256);
+        let stop_flag = Arc::new(AtomicBool::new(false));
+        let stop_flag_clone = stop_flag.clone();
+        let producer_task = tokio::spawn(async move { producer(sender, &stop_flag_clone).await });
+
+        let stop_task = tokio::spawn(async move {
+            sleep(Duration::from_secs(5)).await;
+            stop_flag.store(true, Ordering::Release);
+        });
+
+        Command::new("logger")
+            .args(["testing row"])
+            .output()
+            .await
+            .unwrap();
+
+        let mut log_buffer: Vec<JournalLog> = vec![];
+        while let Some(log) = receiver.recv().await {
+            log_buffer.push(log);
+        }
+
+        let (producer_task_result, stop_task_result) = join!(producer_task, stop_task);
+        producer_task_result.unwrap().unwrap();
+        stop_task_result.unwrap();
+        assert!(
+            !log_buffer
+                .iter()
+                .filter(|&log| &log.message == "testing row")
+                .count()
+                > 0
         );
     }
 }
